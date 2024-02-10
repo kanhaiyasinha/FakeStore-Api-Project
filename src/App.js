d7bb9912f7cca243743405a79100d56733b164f8
Kanhaiya Sinha <sinhakanha21@gmail.com>
2024-02-10 16:02:55 +0530

    Fixed Errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
+import { BrowserRouter, Route , Routes} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
+import { Provider } from 'react-redux';
 import store from './store/store';
-import {Provider} from 'react-redux';
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              path='/' element={<Home />}
+            ></Route>
+            <Route
+              path='/cart' element={<Cart />}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </div>
   );
